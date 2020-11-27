dc6c4fd1b3934bc85dd03b986ad76fce9d2bd435
Ethan Rowe <55393037+ethanrowe5379@users.noreply.github.com>
2020-11-27 15:39:33 +0000

    Create babyClass.cpp

---
+++ babyClass.cpp
@@ -0,0 +1,37 @@
+class baby{
+
+	public:
+		string store[32][32]; //Main Memory
+		int accumulator[32]; //Arimetic Storage
+		int CI[32]; //Current Instruction or Program Counter (Storage of the file input)
+		int PI[32]; //Present Instruction (The fetch from store input)
+
+
+	//constructor and deconstructor
+	baby();
+	~baby();
+
+	//Initialisation, basic running and required converting
+	void initialise();
+	void cpu();
+	int convertBinary();
+	string convertDecimal();
+
+	//Loading file
+	void loadBinaryFromFile();
+
+	// Fetch/Execute
+	void getInstructions();
+	void fetch();
+	void decode();
+	void execute();
+
+	//Commands
+	void JMP();
+	void JRP();
+	void LDN();
+	void STO();
+	void SUB();
+	void CMP();
+	void STP();
+}
