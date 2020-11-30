83ee062ccc3d8b5a61c8039d7cafee354d96ba14
Idris Mahamdi <73561935+idrismahamdi@users.noreply.github.com>
2020-11-30 16:54:45 +0000

    Update babyClass.cpp

--- babyClass.cpp
+++ babyClass.cpp
@@ -1,10 +1,16 @@
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
 class baby{
 
 	public:
-		string store[32][32]; //Main Memory
+		int store[32][32]; //Main Memory
 		int accumulator[32]; //Arimetic Storage
-		int CI[32]; //Current Instruction or Program Counter (Storage of the file input)
-		int PI[32]; //Present Instruction (The fetch from store input)
+		char CI[32]; //Current Instruction or Program Counter (Storage of the file input)
+		char PI[32]; //Present Instruction (The fetch from store input)
 
 
 	//constructor and deconstructor
@@ -14,8 +20,9 @@ class baby{
 	//Initialisation, basic running and required converting
 	void initialise();
 	void cpu();
-	int convertBinary(); //Convert binary to decimal
-	string convertDecimal(); //Convert Decimal to binary
+	int convertBinary(char binary[]); //Convert binary to decimal
+	string convertDecimal(int decimal); //Convert Decimal to binary
+	int convertString(string binary); //Convert String Binary to Int Binary Value
 
 	//Loading file
 	void loadBinaryFromFile(); //normal loading
@@ -23,7 +30,7 @@ class baby{
 	// Fetch/Execute
 	void getInstructions(); //Assembler loading
 	void fetch(); //Fetch code and add to PI
-	void decode(); //Decode the instruction code
+	void decode(int address); //Decode the instruction code
 	void execute();
 
 	//Commands
@@ -34,4 +41,119 @@ class baby{
 	void SUB();
 	void CMP();
 	void STP();
+};
+
+
+
+
+
+int baby::convertBinary(char binary[32])
+{
+	int len = strlen(binary);
+  	int total, counter = 0; 
+      
+  	for(int i=len-1;i>=0;i--){
+   	 
+   	 	if ((int)binary[i]==49)
+   	 	{
+      		total = total + (1*pow(2,counter));
+     	}
+     	
+        counter++;
+     	
+
+ 
+ 	 }
+
+ 	 return total;
+
+
+
+}
+
+
+
+
+string baby::convertDecimal(int decimal)
+{
+	int power=128;
+	string binary = "";
+	int counter =0;
+
+	do
+	{
+		if(decimal<power)
+		{
+			binary = binary+"0";
+		}
+		else
+		{
+			binary = binary+"1";
+			decimal -= power;
+		}
+
+		power /= 2;
+		counter++;
+	} while(counter!=8);
+
+	return binary;
+}
+
+int baby::convertString(string binary){
+	
+	//reads binary into stringstream
+    stringstream convert(binary); 
+
+	//initialise binaryInt
+    int binaryInt = 0; 
+	//read stringstream into binaryInt
+    convert >> binaryInt; 
+  
+    return binaryInt; 
+
+}
+
+
+void baby::fetch()
+{
+
+	int fetch[32];
+	int hold = 0;
+
+	hold = convertBinary(CI);
+
+	for (int i=0;i<32;i++)
+	{
+		fetch[i] = store[hold][i];
+	}
+
+	PI[32] = convertDecimal(fetch);
+
+}
+
+void baby::decode(int address)
+{
+
+	int decode = 0;
+	int decodeValue[3] = {1,2,4};
+
+	for(int index = 0; index <3; index++)
+	{
+		decode = decode + PI[index+13]*decodeValue[index];
+	}
+
+	//  decode = integer value assigned to an instruction
+	// *  returns 		Instruction
+	// *	0				JMP
+	// *	1				JRP
+	// *	2				LDN
+	// *	3				STO
+	// *	4				SUB
+	// *	5				SUB
+	// *	6				CMP
+	// *	7				STP
+	// *
+	
+
+
 }
